e2a4ec0bb3aca0c77de4176ab789d8b4cbba2aec
belyaevDmitry1 <73878729+belyaevDmitry1@users.noreply.github.com>
2020-12-13 22:19:29 +0500

    Update vm.js

--- VM/vm.js
+++ VM/vm.js
@@ -1,103 +1,116 @@
-var fso = new ActiveXObject('Scripting.FileSystemObject');
-var text_prog = fso.OpenTextFile(WSH.Arguments(0)); 
-var s = '';
-while (!text_prog.AtEndOfStream)
-    s += text_prog.ReadLine() + ' ';
-var mem = s.split(' ');
-if(mem[mem.length-1]!='exit')mem[mem.length-1]='exit';
-var ip = 0;
-var argument1 = 0;
-var argument2 = 0;
-var argument3 = 0;
-while (mem[ip] != 'exit') {
-    switch (mem[ip]) {
-        case 'assign':
-            argument1 = parseInt(mem[ip + 1]);
-            argument2 = parseInt(mem[ip + 2]) ;
-            mem[argument2] = argument1;
-            ip += 3
-            break;
-        case 'decr':
-            argument1 = parseInt(mem[ip + 1]);
-            argument2 = parseInt(mem[ip + 2]) ;
-            mem[argument2] -= argument1;
-            ip += 3;
-            break;
-        case 'input':
-            WScript.Echo('Entry') 
-            argument1 = parseInt(mem[ip + 1]) ;
-            mem[argument1] = parseFloat(WScript.StdIn.ReadLine()); //приравниваем к введёному числу
-            ip += 2;
-            break
-        case 'jifeq':
-            argument1 = parseInt(mem[ip + 1]) ;
-            argument2 = parseInt(mem[ip + 2]);
-            argument3 = parseInt(mem[ip + 3]);
-            if (mem[argument1] == argument2) ip = argument3;
-            else ip += 4;
-            break;
-        case 'jump':
-            argument1 = parseInt(mem[ip + 1]);
-            ip = argument1;
-            break;
-        case 'max':
-            argument1 = parseInt(mem[ip + 1]) ;
-            argument2 = parseInt(mem[ip + 2]) ;
-            argument3 = parseInt(mem[ip + 3]) ;
-            if (mem[argument1] >= mem[argument2]) mem[argument3] = mem[argument1];
-            else mem[argument3] = mem[argument2];
-            ip += 4;
-            break;
-        case 'min':
-            argument1 = parseInt(mem[ip + 1]) ;
-            argument2 = parseInt(mem[ip + 2]) ;
-            argument3 = parseInt(mem[ip + 3]) ;
-            if (mem[argument1] <= mem[argument2]) mem[argument3] = mem[argument1];
-            else mem[argument3] = mem[argument2];
-            ip += 4;
-            break;
-        case 'mult':
-            argument1 = parseInt(mem[ip + 1]) ;
-            argument2 = parseInt(mem[ip + 2]) ;
-            argument3 = parseInt(mem[ip + 3]) ;
-            mem[argument3] = mem[argument1] * mem[argument2];
-            ip += 4;
-            break;
-        case 'output':
-            argument = parseInt(mem[ip + 1]);
-            WScript.Echo('Answer: ' + mem[argument])
-            ip += 2
-            break;
-        case 'outexp':
-            WSH.echo('Exeption');
-            ip += 1;
-            break;
-        case 'compar':
-            argument1 = parseInt(mem[ip + 1]) ;
-            argument2 = parseInt(mem[ip + 2]);
-            argument3 = parseInt(mem[ip + 3]);
-            if (mem[argument1] < argument2) ip = argument3;
-            else ip += 4;
-            break;
-        case 'divide':
-            argument1 = parseInt(mem[ip + 1]);
-            argument2 = parseInt(mem[ip + 2]) ;
-            argument3 = parseInt(mem[ip + 3]) ;
-            mem[argument3] = mem[argument1] % mem[argument2];
-            ip += 4;
-            break;
-        case 'sum':
-            argument1 = parseInt(mem[ip + 1]);
-            argument2 = parseInt(mem[ip + 2]) ;
-            argument3 = parseInt(mem[ip + 3]) ;
-            mem[argument3] = mem[argument2] + mem[argument1];
-            ip += 4
-            break;
-        default:
-            for (var count = 0; count < mem.length; count++)
-                WScript.echo('In cell', count, 'stored', mem[count]);
-            WScript.Echo('ne zashlo(')
-            WScript.Quit()
-            break
+const fs = require('fs');
+const readsync = require('readline-sync');
+
+function getPathToProgram() {
+    if (process.argv.length < 3) {
+        console.error('ERROR!\nЕhe path to the program file was not passed');
+        process.exit(-1);
     }
+    return process.argv[2];
 }
+
+function writeProgramToMemory(pathToProgram) {
+    let program = fs.readFileSync(pathToProgram);
+    program = program.toString();
+    return program.split(/\r\n| /);
+}
+
+function runProgram(memory) {
+    let ip = 0;
+    let flag = true;
+    while (flag)
+        switch (memory[ip]) {
+            case 'input':
+                let value = readsync.question('Entered value: ');
+                memory[memory[ip + 1]] = parseFloat(value);
+                ip += 2;
+                break;
+            case 'output':
+                console.log(memory[memory[ip + 1]]);
+                ip += 2;
+                break;
+            case 'set':
+                memory[memory[ip + 1]] = parseFloat(memory[ip + 2]);
+                ip += 3;
+                break;
+            case 'add':
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            case 'sub':
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] - memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            case 'mult':
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            case 'cmp':
+                if (memory[memory[ip + 1]] == memory[memory[ip + 2]])
+                    memory[memory[ip + 3]] = 0;
+                else
+                    memory[memory[ip + 3]] = 1;
+                ip += 4;
+                break;
+            case 'cml':
+                if (memory[memory[ip + 1]] < memory[memory[ip + 2]])
+                    memory[memory[ip + 3]] = 0;
+                else
+                    memory[memory[ip + 3]] = 1;
+                ip += 4;
+                break;
+            case 'cmpl':
+                if (memory[memory[ip + 1]] <= memory[memory[ip + 2]])
+                    memory[memory[ip + 3]] = 0;
+                else
+                    memory[memory[ip + 3]] = 1;
+                ip += 4;
+                break;
+            case 'cmm':
+                if (memory[memory[ip + 1]] > memory[memory[ip + 2]])
+                    memory[memory[ip + 3]] = 0;
+                else
+                    memory[memory[ip + 3]] = 1;
+                ip += 4;
+                break;
+            case 'cmpm':
+                if (memory[memory[ip + 1]] >= memory[memory[ip + 2]])
+                    memory[memory[ip + 3]] = 0;
+                else
+                    memory[memory[ip + 3]] = 1;
+                ip += 4;
+                break;
+            case 'jmp':
+                ip = parseFloat(memory[ip + 1]);
+                break;
+            case 'jz':
+                if (memory[memory[ip + 1]] == 0)
+                    ip = parseFloat(memory[ip + 2]);
+                else
+                    ip += 3;
+                break;
+            case 'jnz':
+                if (memory[memory[ip + 1]] != 0)
+                    ip = parseFloat(memory[ip + 2]);
+                else
+                    ip += 3;
+                break;
+            case 'exit':
+                flag = false;
+                break;
+            default:
+                console.log('Error(s) in the program code!');
+                process.exit(-1);
+        }
+}
+
+function main() {
+    let pathToProgram = getPathToProgram();
+    let memory = writeProgramToMemory(pathToProgram);
+    runProgram(memory);
+    console.log('\n---------------------------------');
+    console.log('The pogram worked without errors!');
+}
+
+
+main();

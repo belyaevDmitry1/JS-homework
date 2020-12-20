2797d1e4051ca1b0a0fe1a9f79fcc2d917f4a308
belyaevDmitry1 <73878729+belyaevDmitry1@users.noreply.github.com>
2020-12-20 20:29:55 +0500

    Update vm.js

--- VM/vm.js
+++ VM/vm.js
@@ -1,116 +1,97 @@
-const fs = require('fs');
-const readsync = require('readline-sync');
+let fs = require ('fs');
+let arg = process.argv;
+let mem = new Array();
 
-function getPathToProgram() {
-    if (process.argv.length < 3) {
-        console.error('ERROR!\nЕhe path to the program file was not passed');
-        process.exit(-1);
-    }
-    return process.argv[2];
-}
+let text = fs.readFileSync('nod.jss');
+text=text.toString();
 
-function writeProgramToMemory(pathToProgram) {
-    let program = fs.readFileSync(pathToProgram);
-    program = program.toString();
-    return program.split(/\r\n| /);
-}
+mem=text.split(/\r\n| /);
 
-function runProgram(memory) {
-    let ip = 0;
-    let flag = true;
-    while (flag)
-        switch (memory[ip]) {
+
+for (let i=0;i<mem.length;i++)
+	console.log(i,mem[i]);
+ip=0;
+flag=true;
+while (flag)
+	 switch (mem[ip]) {
             case 'input':
                 let value = readsync.question('Entered value: ');
-                memory[memory[ip + 1]] = parseFloat(value);
+                mem[mem[ip + 1]] = parseFloat(value);
                 ip += 2;
                 break;
             case 'output':
-                console.log(memory[memory[ip + 1]]);
+                console.log(mem[mem[ip + 1]]);
                 ip += 2;
                 break;
             case 'set':
-                memory[memory[ip + 1]] = parseFloat(memory[ip + 2]);
+                memory[mem[ip + 1]] = parseFloat(mem[ip + 2]);
                 ip += 3;
                 break;
             case 'add':
-                memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                memory[mem[ip + 3]] = mem[mem[ip + 1]] + mem[mem[ip + 2]];
                 ip += 4;
                 break;
             case 'sub':
-                memory[memory[ip + 3]] = memory[memory[ip + 1]] - memory[memory[ip + 2]];
+                mem[memory[ip + 3]] = mem[mem[ip + 1]] - mem[mem[ip + 2]];
                 ip += 4;
                 break;
             case 'mult':
-                memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                mem[mem[ip + 3]] = mem[mem[ip + 1]] * mem[mem[ip + 2]];
                 ip += 4;
                 break;
             case 'cmp':
-                if (memory[memory[ip + 1]] == memory[memory[ip + 2]])
-                    memory[memory[ip + 3]] = 0;
+                if (mem[mem[ip + 1]] == mem[mem[ip + 2]])
+                    mem[mem[ip + 3]] = 0;
                 else
-                    memory[memory[ip + 3]] = 1;
+                    mem[mem[ip + 3]] = 1;
                 ip += 4;
                 break;
             case 'cml':
-                if (memory[memory[ip + 1]] < memory[memory[ip + 2]])
-                    memory[memory[ip + 3]] = 0;
+                if (mem[mem[ip + 1]] < mem[mem[ip + 2]])
+                    mem[mem[ip + 3]] = 0;
                 else
-                    memory[memory[ip + 3]] = 1;
+                    mem[mem[ip + 3]] = 1;
                 ip += 4;
                 break;
             case 'cmpl':
-                if (memory[memory[ip + 1]] <= memory[memory[ip + 2]])
-                    memory[memory[ip + 3]] = 0;
+                if (mem[mem[ip + 1]] <= mem[mem[ip + 2]])
+                    mem[mem[ip + 3]] = 0;
                 else
-                    memory[memory[ip + 3]] = 1;
+                    mem[mem[ip + 3]] = 1;
                 ip += 4;
                 break;
             case 'cmm':
-                if (memory[memory[ip + 1]] > memory[memory[ip + 2]])
-                    memory[memory[ip + 3]] = 0;
+                if (mem[mem[ip + 1]] > mem[mem[ip + 2]])
+                    mem[mem[ip + 3]] = 0;
                 else
-                    memory[memory[ip + 3]] = 1;
+                    mem[mem[ip + 3]] = 1;
                 ip += 4;
                 break;
             case 'cmpm':
-                if (memory[memory[ip + 1]] >= memory[memory[ip + 2]])
-                    memory[memory[ip + 3]] = 0;
+                if (mem[mem[ip + 1]] >= mem[mem[ip + 2]])
+                    mem[mem[ip + 3]] = 0;
                 else
-                    memory[memory[ip + 3]] = 1;
+                    mem[mem[ip + 3]] = 1;
                 ip += 4;
                 break;
             case 'jmp':
-                ip = parseFloat(memory[ip + 1]);
+                ip = parseFloat(mem[ip + 1]);
                 break;
             case 'jz':
-                if (memory[memory[ip + 1]] == 0)
-                    ip = parseFloat(memory[ip + 2]);
+                if (mem[mem[ip + 1]] == 0)
+                    ip = parseFloat(mem[ip + 2]);
                 else
                     ip += 3;
                 break;
             case 'jnz':
-                if (memory[memory[ip + 1]] != 0)
-                    ip = parseFloat(memory[ip + 2]);
+                if (mem[mem[ip + 1]] != 0)
+                    ip = parseFloat(mem[ip + 2]);
                 else
                     ip += 3;
                 break;
             case 'exit':
                 flag = false;
                 break;
-            default:
-                console.log('Error(s) in the program code!');
-                process.exit(-1);
-        }
-}
-
-function main() {
-    let pathToProgram = getPathToProgram();
-    let memory = writeProgramToMemory(pathToProgram);
-    runProgram(memory);
-    console.log('\n---------------------------------');
-    console.log('The pogram worked without errors!');
-}
-
-
-main();
+	}
+	for (let i=0;i<mem.length;i++)
+	console.log(i,mem[i]);

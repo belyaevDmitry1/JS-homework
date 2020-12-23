a9732f5a280d8d632b8c66d24b4d4aa725607984
belyaevDmitry1 <73878729+belyaevDmitry1@users.noreply.github.com>
2020-12-23 19:48:48 +0500

    Update automat.js

--- Automata/automat.js
+++ Automata/automat.js
@@ -1,124 +1,42 @@
-const fs = require('fs');
-
-Array.prototype.contains = function (necessaryElement) {
-    for (let element in this)
-        if (element == necessaryElement)
-            return true;
-    return false;
+let fs = require('fs');
+let arg = process.argv;
+let S = fs.readFileSync('ВойнаИМир.txt');
+let T = "князь";
+m = T.length;
+alph = new Array()
+//Определяем алфавит строки T
+for (i = 0; i < m; i++)
+    alph[T.charAt(i)] = 0;
+//В двумерном массиве del будем хранить таблицу переходов
+del = new Array(m + 1)
+for (j = 0; j <= m; j++)
+    del[j] = new Array();
+//Инициализируем таблицу переходов
+for (i in alph)
+    del[0][i] = 0;
+//Формируем таблицу переходов
+for (j = 0; j < m; j++) {
+    prev = del[j][T.charAt(j)];
+    del[j][T.charAt(j)] = j + 1;
+    for (i in alph)
+        del[j + 1][i] = del[prev][i];
 }
-
-function getParametersOfProgram() {
-    let pathToFileWithString;
-    let pathToWritableFile;
-    let substring;
-    process.argv.forEach((value, index) => {
-        if (index == 2)
-            pathToFileWithString = value;
-        if (index == 3)
-            pathToWritableFile = value;
-        if (index == 4)
-            substring = value;
-    });
-
-    if (!pathToFileWithString) {
-        console.error('ERROR!\nThe path to the readable file was not specified');
-        process.exit(-1);
+//Выводим таблицу переходов
+for (j = 0; j <= m; j++) {
+    out = " ";
+    for (i in alph)
+        out += del[j][i] + " ";
+    console.log(out);
+}
+S = S.toString();
+let position = 0;
+for (let i = 0; i < S.length; i++) {
+    if (S[i] in alph) {
+        position = del[position][S[i]];
     }
-    if (!pathToWritableFile) {
-        console.error('ERROR!\nThe path to the writable file was not specified');
-        process.exit(-1);
+    else
+        position = 0;
+    if (position == T.length) {
+        console.log(i - position + 1);
     }
-    if (!substring) {
-        console.error('ERROR!\nThe substring was not specified');
-        process.exit(-1);
-    }
-
-    return new Array(pathToFileWithString, pathToWritableFile, substring);
 }
-
-function findAllOccurrencesOfSubstringInString(pathToFileWithString, pathToWritableFile, substring) {
-    fs.readFile(pathToFileWithString, (err, data) => {
-        if (err) {
-            if (err.code == 'ENOENT') {
-                console.error('ERROR!\nThe path ' + pathToFileWithString + ' does not exist');
-                process.exit(-1);
-            }
-            if (err.code == 'EISDIR') {
-                console.error('ERROR!\nPath to file expected\n(the path to the directory is specified)');
-                process.exit(-1);
-            }
-            if (err.code == 'EACCES') {
-                console.error('ERROR!\nAccess is denied to the file ' + pathToFileWithString);
-                process.exit(-1);
-            }
-        }
-
-        let start = new Date();
-        let occurrences = findSubstringWithAutomat(data.toString(), substring);
-        let end = new Date();
-        console.log("Operating time: ", end - start, " milliseconds");
-
-        let indexs = "";
-        for (let i = 0; i < occurrences.length; i++)
-            indexs += occurrences[i] + '\n';
-
-        fs.writeFile(pathToWritableFile, indexs, (err) => {
-            if (err) {
-                if (err.code == 'EACCES')
-                    console.error('ERROR!\nAccess is denied to the file ' + pathToWritableFile);
-                process.exit(-1);
-            }
-        });
-    })
-}
-
-function findSubstringWithAutomat(string, substring) {
-    let alphabet = getAlphabetFor(substring);
-    let jumpTable = getJumpTableFor(substring);
-    let currentState = 0;
-    let occurrences = new Array();
-
-    for (let i = 0; i < string.length; i++) {
-        if (alphabet.contains(string[i]))
-            currentState = jumpTable[currentState][string.charAt(i)];
-        else
-            currentState = 0;
-        if (currentState == jumpTable.length - 1)
-            occurrences.push(i - currentState + 2);
-    }
-    return occurrences;
-}
-
-function getAlphabetFor(string) {
-    let alphabet = new Array();
-    for (let i = 0; i < string.length; i++)
-        alphabet[string.charAt(i)] = 0;
-    return alphabet;
-}
-
-function getJumpTableFor(string) {
-    let alphabet = getAlphabetFor(string);
-    let jumpTable = new Array(string.length + 1);
-    for (j = 0; j <= string.length; j++)
-        jumpTable[j] = new Array();
-    for (i in alphabet)
-        jumpTable[0][i] = 0;
-    for (j = 0; j < string.length; j++) {
-        let prev = jumpTable[j][string.charAt(j)];
-        jumpTable[j][string.charAt(j)] = j + 1;
-        for (i in alphabet)
-            jumpTable[j + 1][i] = jumpTable[prev][i];
-    }
-    return jumpTable;
-}
-
-function main() {
-    let parametersOfProgram = getParametersOfProgram();
-    let pathToFileWithString = parametersOfProgram[0];
-    let pathToWritableFile = parametersOfProgram[1];
-    let substring = parametersOfProgram[2];
-    findAllOccurrencesOfSubstringInString(pathToFileWithString, pathToWritableFile, substring);
-}
-
-
-main();
